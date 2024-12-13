71ce15525c4353b407a413b25d0708655ec46742
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-13 18:27:56 -0300

    feat: added auth homa component and router configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth-home',
+        pathMatch: 'full',
+        loadComponent: () => import('./auth/pages/auth-home/auth-home.component').then(m => m.AuthHomeComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'auth-home'
+    }
+];
