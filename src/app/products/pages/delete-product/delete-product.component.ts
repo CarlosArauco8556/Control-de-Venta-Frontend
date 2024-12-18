988aa7de63460de218c23424a5e0f27025550ce8
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-18 02:13:03 -0300

    feat: deploy the deleted product and errors.

--- src/app/products/pages/delete-product/delete-product.component.ts
+++ src/app/products/pages/delete-product/delete-product.component.ts
@@ -4,11 +4,12 @@ import { GetProductsComponent } from '../get-products/get-products.component';
 import { GetProducts } from '../../interfaces/getProducts';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'products-delete-product',
   imports: [HttpClientModule, CommonModule],
-  providers: [ProductService, GetProductsComponent],
+  providers: [ProductService, GetProductsComponent, FormsModule],
   templateUrl: './delete-product.component.html',
   styleUrl: './delete-product.component.css'
 })
@@ -16,11 +17,55 @@ export class DeleteProductComponent {
   public productService: ProductService = inject(ProductService)
   public getProductsComponent: GetProductsComponent = inject(GetProductsComponent);
   public product: GetProducts = { name: '', price: 0, discountPercentage: 0, stock: 0, stockMin: 0 };
+  public isModalVisible: boolean = false;
+  public productIsDeleted: boolean = false;
+  public error: boolean = false;
+  public errorMessage: string[] = [];
+  
 
-  deleteProduct(input: string)
+  async deleteProduct(input: string)
   {
-    const id = parseInt(input);
-    this.productService.deleteProduct(id);
-    this.getProductsComponent.getProducts();
+    try
+    {
+      const productId = parseInt(input);
+      const productDeleted = await this.productService.deleteProduct(productId);
+      this.product = productDeleted;
+      this.getProductsComponent.getProducts();
+      if (productDeleted !== null) this.productIsDeleted = true;
+      console.log(this.productIsDeleted);
+      setTimeout(() => {
+        this.productIsDeleted = false;
+        console.log(this.productIsDeleted);
+      }, 3000); // Wait 4 seconds
+      this.error = false;
+    } catch (error){
+      console.log("Error in DeleteProduct", error);
+      this.handleServerError(error);
+    }
+  }
+
+  private handleServerError(error: any) {
+    console.error('Server error:', error);
+
+    if (error.error?.errors) {
+      this.errorMessage = [
+        ...(Object.values(error.error.errors)
+          .flat()
+          .filter((e): e is string => typeof e === 'string'))
+      ];
+    } else if (typeof error.error === 'string') {
+      this.errorMessage.push(error.error);
+    } else {
+      this.errorMessage.push('An unexpected error occurred. Please try again later.');
+    }
+    this.error = true;
+    setTimeout(() => {
+      this.error = false;
+    }, 2000); // Wait 3 seconds
+  }
+
+  getLastError(): string
+  {
+    return this.errorMessage[this.errorMessage.length - 1];
   }
 }
--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -16,7 +16,7 @@ export class ProductService {
   async getAllProducts(queryParamsI: QueryParams): Promise<GetProducts[]>{
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiIwMWEwODQ1MS00N2E3LTQwOGMtOGRiNC1iOWRmZDQ4Mzc4YjciLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQyMjg5OTQsImV4cCI6MTczNDMxNTM5NCwiaWF0IjoxNzM0MjI4OTk0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.ShZFPIGhy4y2WBvn6EkeWX4FmzHqnQlrc3PltoXpxPQbArUf0PZ1nU6UOZ92poLULbRWBLdcA5ENbQ5y-RSMHA';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       let queryParams = new HttpParams()
         if (queryParamsI.textFilter) queryParams = queryParams.set('textFilter', queryParamsI.textFilter);
@@ -41,7 +41,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiIwMWEwODQ1MS00N2E3LTQwOGMtOGRiNC1iOWRmZDQ4Mzc4YjciLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQyMjg5OTQsImV4cCI6MTczNDMxNTM5NCwiaWF0IjoxNzM0MjI4OTk0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.ShZFPIGhy4y2WBvn6EkeWX4FmzHqnQlrc3PltoXpxPQbArUf0PZ1nU6UOZ92poLULbRWBLdcA5ENbQ5y-RSMHA';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.post<GetProducts>(this.baseUrl1, product, { headers: headers }));
       return Promise.resolve(response);
@@ -57,7 +57,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiIwMWEwODQ1MS00N2E3LTQwOGMtOGRiNC1iOWRmZDQ4Mzc4YjciLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQyMjg5OTQsImV4cCI6MTczNDMxNTM5NCwiaWF0IjoxNzM0MjI4OTk0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.ShZFPIGhy4y2WBvn6EkeWX4FmzHqnQlrc3PltoXpxPQbArUf0PZ1nU6UOZ92poLULbRWBLdcA5ENbQ5y-RSMHA';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.put<GetProducts>(`${this.baseUrl1}/${id}`, product, { headers: headers}));
       return Promise.resolve(response);
@@ -73,7 +73,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiIwMWEwODQ1MS00N2E3LTQwOGMtOGRiNC1iOWRmZDQ4Mzc4YjciLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQyMjg5OTQsImV4cCI6MTczNDMxNTM5NCwiaWF0IjoxNzM0MjI4OTk0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.ShZFPIGhy4y2WBvn6EkeWX4FmzHqnQlrc3PltoXpxPQbArUf0PZ1nU6UOZ92poLULbRWBLdcA5ENbQ5y-RSMHA';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.delete<GetProducts>(`${this.baseUrl1}/${id}`, { headers: headers}));
       return Promise.resolve(response);
