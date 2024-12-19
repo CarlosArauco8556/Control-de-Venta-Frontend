86b46280cac459479348c3e7a30401667f5620f0
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-18 23:05:00 -0300

    feat: finished update product.

--- src/app/products/pages/update-product/update-product.component.ts
+++ src/app/products/pages/update-product/update-product.component.ts
@@ -1,11 +1,132 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { GetProductsComponent } from '../get-products/get-products.component';
+import { GetProducts } from '../../interfaces/getProducts';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'products-update-product',
-  imports: [],
+  imports: [HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule],
+  providers: [ProductService, GetProductsComponent],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
 export class UpdateProductComponent {
+  productService: ProductService = inject(ProductService);
+  getProductsComponent: GetProductsComponent = inject(GetProductsComponent);
+  product: GetProducts = { name: '', price: 0, discountPercentage: 0, stock: 0, stockMin: 0};
+  errorMessages: string[] = [];
+  error: boolean = false;
+  productIsUpdated: boolean = false;
+  forms: FormGroup = new FormGroup({});
+  id: number = 0;
+
+  constructor(private FormBuilder: FormBuilder){}
+
+  ngOnInit(){
+    this.createForm();
+  }
+
+  createForm(){
+    this.forms = this.FormBuilder.group({
+      id: ['', [Validators.required, Validators.min(0)]],
+      name: ['', [Validators.required]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      discountPercentage: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
+      stock: ['', [Validators.required, Validators.min(0)]],
+      stockMin: ['', [Validators.required, Validators.min(0)]]
+    })
+  }
+
+  async updateProduct(){
+    this.errorMessages = [];
+    if(this.forms.invalid){
+      this.handleFormErrors();
+      return;
+    }
+    try
+    {
+      this.product.name = this.forms.value.name;
+      this.product.price = this.forms.value.price;
+      this.product.discountPercentage = this.forms.value.discountPercentage;
+      this.product.stock = this.forms.value.stock;
+      this.product.stockMin = this.forms.value.stockMin;
+
+      this.id = this.forms.value.id;
+
+      const productUpdated = await this.productService.updateProduct(this.id, this.product);
+      if(productUpdated !== null){
+        this.productIsUpdated = true;
+        this.getProductsComponent.getProducts();
+        this.forms.reset();
+      }
+      setTimeout(() => {
+        this.productIsUpdated = false;
+      }, 3000);
+
+    } catch (error){
+      console.log("Error in UpdateProduct", error);
+      this.handleServerError(error);
+    }
+  }
+
+  handleFormErrors(){
+    if(this.forms.controls['id'].invalid){
+      this.errorMessages.push('ID is required');
+
+      this.handleError();
+    }
+    if(this.forms.controls['name'].invalid){
+      this.errorMessages.push('Name is required');
+      
+      this.handleError();
+    }
+    if(this.forms.controls['price'].invalid){
+      this.errorMessages.push('Price is required and must be greater than 0');
+      
+      this.handleError();
+    }
+    if(this.forms.controls['discountPercentage'].invalid){
+      this.errorMessages.push('Discount Percentage is required and must be between 0 and 100');
+      
+      this.handleError();
+    }
+    if(this.forms.controls['stock'].invalid){
+      this.errorMessages.push('Stock is required and must be greater than 0');
+      
+      this.handleError();
+    }
+    if(this.forms.controls['stockMin'].invalid){
+      this.errorMessages.push('Stock Min is required and must be greater than 0');
+      
+      this.handleError();
+    }
+  }
+
+  handleServerError(error: any){
+    console.error('Server error:', error);
+
+    if (error.error?.errors) {
+      this.errorMessages = [
+        ...(Object.values(error.error.errors)
+          .flat()
+          .filter((e): e is string => typeof e === 'string'))
+      ];
+    } else if (typeof error.error === 'string') {
+      this.errorMessages.push(error.error);
+    } else {
+      this.errorMessages.push('An unexpected error occurred. Please try again later.');
+    }
+    this.handleError();
+  }
+
+  handleError(){
+    this.error = true;
+    setTimeout(() => {
+      this.error = false;
+    }, 2000);
+  }
 
 }
--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -16,7 +16,7 @@ export class ProductService {
   async getAllProducts(queryParamsI: QueryParams): Promise<GetProducts[]>{
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiI0ZDM0NTRlZC01NjRiLTQ5NWItOTEwNi05MGNlNWU5NDU2Y2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ1NzM1NzIsImV4cCI6MTczNDY1OTk3MiwiaWF0IjoxNzM0NTczNTcyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.XeurqhtESkfljlnulW-1cjE_KjHHRL2aEbv6vZoan57VuNAEhfB33Hmld9qazf7SLHNGTDh9o4oq0nyFRPBVvw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       let queryParams = new HttpParams()
         if (queryParamsI.textFilter) queryParams = queryParams.set('textFilter', queryParamsI.textFilter);
@@ -41,7 +41,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiI0ZDM0NTRlZC01NjRiLTQ5NWItOTEwNi05MGNlNWU5NDU2Y2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ1NzM1NzIsImV4cCI6MTczNDY1OTk3MiwiaWF0IjoxNzM0NTczNTcyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.XeurqhtESkfljlnulW-1cjE_KjHHRL2aEbv6vZoan57VuNAEhfB33Hmld9qazf7SLHNGTDh9o4oq0nyFRPBVvw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.post<GetProducts>(this.baseUrl1, product, { headers: headers }));
       return Promise.resolve(response);
@@ -57,7 +57,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiI0ZDM0NTRlZC01NjRiLTQ5NWItOTEwNi05MGNlNWU5NDU2Y2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ1NzM1NzIsImV4cCI6MTczNDY1OTk3MiwiaWF0IjoxNzM0NTczNTcyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.XeurqhtESkfljlnulW-1cjE_KjHHRL2aEbv6vZoan57VuNAEhfB33Hmld9qazf7SLHNGTDh9o4oq0nyFRPBVvw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.put<GetProducts>(`${this.baseUrl1}/${id}`, product, { headers: headers}));
       return Promise.resolve(response);
@@ -73,7 +73,7 @@ export class ProductService {
   {
     try
     {
-      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiJjYTJlNWJjNC1hODMzLTQyOWEtYjA2Mi1hNWI1ZGZjZTk0MmQiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ0ODM0MjQsImV4cCI6MTczNDU2OTgyNCwiaWF0IjoxNzM0NDgzNDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.kqz39-Of37FWjbKMAJR6aVsaf5UvaxLVrlnwzNCLm-KoAVQx-Yx9pV21VvVrOnKAaI9eWKc23NugmYhgFTWcSw';
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGlkd20uY2wiLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJqdGkiOiI0ZDM0NTRlZC01NjRiLTQ5NWItOTEwNi05MGNlNWU5NDU2Y2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3MzQ1NzM1NzIsImV4cCI6MTczNDY1OTk3MiwiaWF0IjoxNzM0NTczNTcyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUwMCJ9.XeurqhtESkfljlnulW-1cjE_KjHHRL2aEbv6vZoan57VuNAEhfB33Hmld9qazf7SLHNGTDh9o4oq0nyFRPBVvw';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.delete<GetProducts>(`${this.baseUrl1}/${id}`, { headers: headers}));
       return Promise.resolve(response);
