e4ef0bfecdfc128cd1ffea0b40c5089a5bf18ef9
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-12-19 11:00:15 -0300

    feat: add new supplier page created with html and ts

--- src/app/supplier/pages/add-supplier/add-supplier.component.ts
+++ src/app/supplier/pages/add-supplier/add-supplier.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SupplierServiceService } from '../../services/supplier-service.service';
+import { Supplier } from '../../interfaces/supplier';
 
 @Component({
   selector: 'app-add-supplier',
@@ -7,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './add-supplier.component.css'
 })
 export class AddSupplierComponent {
+  supplier = { name: '', rut: '', email: '', phone: '' };
+  errorMessage: string = '';
+  successMessage: string = '';
 
+  constructor(private supplierService: SupplierServiceService) {}
+
+  onSubmit(): void {
+    this.supplierService.getAllSuppliers().subscribe((suppliers) => {
+      const existingSupplier = suppliers.find((s: { name: string; }) => s.name === this.supplier.name);
+
+      if (existingSupplier) {
+        this.errorMessage = 'El suministrador ya existe.';
+        this.successMessage = '';
+        return;
+      }
+
+      this.supplierService.addSupplier(this.supplier).subscribe(() => {
+        this.successMessage = 'Suministrador añadido correctamente.';
+        this.errorMessage = '';
+        this.supplier = { name: '', rut: '', email: '', phone: '' }; // Limpiar formulario
+      });
+    });
+  }
 }
